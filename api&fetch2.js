d5ed81fd41ca5b5bdd7043e300577b85ea60bbba
nmvsaikumarb3 <42762591+nmvsaikumarb3@users.noreply.github.com>
2021-07-20 15:42:02 +0530

    Update api&fetch2.js

--- api&fetch2.js
+++ api&fetch2.js
@@ -2,4 +2,4 @@ const root='https://jsonplaceholder.typicode.com/posts/1';
 
 fetch(root,{method:"GET"})
 .then(response =>response.json())
-.then(json =>console.log());
+.then(json =>console.log(json));
