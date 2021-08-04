989084a25891068407bc9d45b2d1aa83126b8824
nmvsaikumarb3 <42762591+nmvsaikumarb3@users.noreply.github.com>
2021-08-04 13:04:53 +0530

    countdownChamp application

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import './index.css';
+import ReactDom from 'react-dom';
+import App from './App';
 
-
-ReactDOM.render(<App/>,document.getElementById('root')); //<App/> it is the self enclosing tag if dont want write anything in the middle of the tags
-
-new Promise((resolve ,reject)=>
-    {
-        return reject(new Error('No bears'));
-        setTimeout(()=>
-        {
-            resolve('Bears,Beets,Battlerstar Galactica');
-        },2000);
-     } )
-    .then(quote=>{
-        console.log(quote);
-        
-    })
-    .catch(error=>console.log('error',error));
+ReactDom.render(
+  <App />,
+  document.getElementById('root')
+);
+       
